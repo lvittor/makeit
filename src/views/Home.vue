e8b641ba4516bcee4e1524f8e82e0f87a8cda2f8
lvittor <lvittor@itba.edu.ar>
2021-09-24 17:35:39 -0300

    rm components, add login view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,5 @@
 <template>
   <v-div>
-    <Card />
-    <Rutina />
-    <EmailTF />
-
-    <PassTF />
-
-    <DataTable />
-    <Star />
-    <!-- <TimePicker /> -->
-    <Alert />
-    <DefaultB />
-    <OutlinedB />
-    <RoundB />
-    <OutlinedRoundB />
-    <IconB />
-    <OutlinedIconB />
-    <EditPencil />
-    <AvatarIcon />
-    <AlertClosingRutine />
   </v-div>
 </template>
 
@@ -27,24 +8,6 @@ export default {
   name: "Home",
 
   components: {
-    Card: () => import("../components/Card"),
-    DataTable: () => import("../components/DataTable"),
-    Star: () => import("../components/Star"),
-    // TimePicker: () => import("../components/TimePicker"),
-    Alert: () => import("../components/Alert"),
-    AvatarIcon: () => import("../components/AvatarIcon"),
-    Rutina: () => import("../components/Rutina"),
-    EmailTF: () => import("../components/TextFields/EmailTF"),
-    PassTF: () => import("../components/TextFields/PassTF"),
-    EditPencil: () => import("../components/EditPencil"),
-    AlertClosingRutine: () => import("../components/AlertClosingRutine"),
-
-    DefaultB: () => import("../components/Buttons/DefaultB"),
-    OutlinedB: () => import("../components/Buttons/OutlinedB"),
-    RoundB: () => import("../components/Buttons/RoundB"),
-    OutlinedRoundB: () => import("../components/Buttons/OutlinedRoundB"),
-    IconB: () => import("../components/Buttons/IconB"),
-    OutlinedIconB: () => import("../components/Buttons/OutlinedIconB"),
   
   },
 };
