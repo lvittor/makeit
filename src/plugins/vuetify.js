8d0c9b52c09c09a6d9f19f28d197bb3e24097dc7
lvittor <lvittor@itba.edu.ar>
2021-10-06 15:08:51 -0300

    fix: add es lang

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,26 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
+import colors from "vuetify/lib/util/colors";
+import es from "../locale/es"
+
 
 Vue.use(Vuetify);
 
-import colors from "vuetify/lib/util/colors";
+
+
+Vue.component("signin", {
+  methods: {
+    changeLocale() {
+      this.$vuetify.lang.current = "es";
+    },
+  },
+});
 
 export default new Vuetify({
+  lang: {
+    locales: { es },
+    current: "es",
+  },
   theme: {
     themes: {
       light: {
