087a0958773c2bd7245f889bedead7232f00e045
lvittor <lvittor@itba.edu.ar>
2021-10-06 16:01:42 -0300

    fix: change password text field component

--- src/components/TextFields/Password.vue
+++ src/components/TextFields/Password.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/components/TextFields/Password.vue
 <template>
   <v-text-field
     :label="label"
@@ -36,34 +35,3 @@ export default {
   },
 };
 </script>
-=======
-<template>
-  <v-text-field
-    v-model="password"
-    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-    :rules="[rules.Required]"
-    :type="show1 ? 'text' : 'password'"
-    name="Password"
-    label="Contraseña"
-    outlined
-    counter
-    @click:append="show1 = !show1"
-  ></v-text-field>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      show1: false,
-      password: "",
-      rules: {
-        required: (value) => !!value || "Required.",
-        min: (v) => v.length >= 8 || "Min 8 characters",
-        emailMatch: () => `The email and password you entered don't match`,
-      },
-    };
-  },
-};
-</script>
->>>>>>> fb728ad998c249d2849233adab770627b6658cd5:src/components/TextFields/PassTF.vue
