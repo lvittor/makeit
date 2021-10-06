926fa206b59d8b6f7389df8416553009f66802f9
lvittor <lvittor@itba.edu.ar>
2021-10-06 15:06:44 -0300

    develop: add locale

---
+++ src/locale/es.js
@@ -0,0 +1,27 @@
+import { es } from "vuetify/lib/locale";
+
+export default {
+  ...es,
+
+  auth: {
+    "sign-in": {
+      label: "Email",
+      title: "Iniciar sesión",
+      signin: "Iniciar sesión",
+      forgotpassword: "¿Olvidaste tu contraseña?",
+    },
+    "sign-up": {
+      create: "Crear cuenta",
+      email: "Email",
+      firstname: "Nombre",
+      lastname: "Apellido",
+      username: "Nombre de usuario",
+      password: "Contraseña",
+      confirmpassword: "Confirmación",
+      "password-props":
+        "Usa 8 o más caracteres con una mezcla de letras, números y símbolos",
+      signup: "Registrarme",
+    },
+  },
+  login: {},
+};
