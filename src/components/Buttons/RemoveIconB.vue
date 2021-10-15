45bcb61a7cc1dcdf0a29a0dcb93d86516d826b6e
festevezz <festevez@itba.edu.ar>
2021-10-14 22:58:54 -0300

    hotfix: add delete confirm dialog

--- src/components/Buttons/RemoveIconB.vue
+++ src/components/Buttons/RemoveIconB.vue
@@ -1,7 +1,50 @@
 <template>
-  <v-btn x-large  color="primary" fab @click="deleteRoutine(routineID)">
-    <v-icon>mdi-delete</v-icon>
-  </v-btn>
+    <div class="d-flex justify-center">
+        <v-dialog
+            transition="dialog-bottom-transition"
+            max-width="600"
+        >
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    v-bind="attrs"
+                    v-on="on"
+                    x-large 
+                    color="primary" 
+                    fab 
+                >
+                    <v-icon>mdi-delete</v-icon>
+                </v-btn>
+            </template>
+            <template v-slot:default="dialog">
+                <v-alert
+                    type="info"
+                    icon="mdi-information-outline"
+                    class="ma-0"
+                    color="secondary"
+                    prominent
+                    border="left"
+                >
+                    ¿Está seguro que desea eliminar la rutina?
+                </v-alert>
+                <v-row class="ma-0">
+                    <v-col
+                        class="pa-0"
+                    >
+                        <v-btn color="black" class="white--text" block @click="dialog.value = false">
+                            CANCELAR
+                        </v-btn>
+                    </v-col>
+                    <v-col
+                        class="pa-0"
+                    >
+                        <v-btn color="black" class="white--text" block @click="deleteRoutine(routineID), dialog.value = false">
+                            CONFIRMAR
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </template>
+        </v-dialog>
+    </div>
 </template>
 
 <script>
@@ -19,7 +62,6 @@ export default {
         }),
 
         async deleteRoutine(routineID){
-            alert("Estas seguro de borrar la rutina con id: " + routineID)
             await this.$delete({id: routineID});
             
             this.$root.$emit("update")
